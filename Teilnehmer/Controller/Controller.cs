e13ae5b3d5f844c4ffd0031ec47bc259b8e37d65
Monika <mm@froso.de>
2021-02-04 10:45:39 +0100

    just a setup2

---
+++ Teilnehmer/Controller/Controller.cs
@@ -0,0 +1,21 @@
+namespace Teilnehmer
+{
+    public class Controller
+    {
+        #region Attributes
+
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region Constructors
+
+        #endregion
+
+        #region Methods
+
+        #endregion
+    }
+}
---
+++ Teilnehmer/View/UserInterface.cs
@@ -0,0 +1,21 @@
+namespace Teilnehmer
+{
+    public class UserInterface
+    {
+        #region Attributes
+
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region Constructors
+
+        #endregion
+
+        #region Methods
+
+        #endregion
+    }
+}
